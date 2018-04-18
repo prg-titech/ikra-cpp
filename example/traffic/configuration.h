3e0dec1a82e1c9e828780c69c72a8f2d483ace37
Matthias Springer <me@matthiasspringer.de>
2018-04-18 13:08:14 +0900

    Add comments.

--- example/traffic/configuration.h
+++ example/traffic/configuration.h
@@ -4,22 +4,26 @@
 class Cell;
 class SharedSignalGroup;
 
+// Statistics for benchmark
 static const uint32_t kNumIterations = 2000;
 static const uint32_t kNumBenchmarkRuns = 5;
 
+// Number of objects.
 static const uint32_t kNumCells = 5856393;
 static const uint32_t kNumCars = 250000;
 static const uint32_t kNumSharedSignalGroups = 344418;
 static const uint32_t kNumTrafficLights = 50972;
 static const uint32_t kNumPriorityCtrls = 6000;
 
-static const uint32_t kArrInlineOutgoingCells = 4;  // 4
-static const uint32_t kArrInlineIncomingCells = 8;  // 8
-static const uint32_t kArrInlinePath = 5;            // 5
+// Inlining sizes of arrays.
+static const uint32_t kArrInlineOutgoingCells = 4;
+static const uint32_t kArrInlineIncomingCells = 8;
+static const uint32_t kArrInlinePath = 10;
 static const uint32_t kArrInlineSignalGroupCells = 4;
 static const uint32_t kArrInlineTrafficLightSignalGroups = 4;
 static const uint32_t kArrInlinePriorityCtrlSignalGroups = 4;
 
+// Sizes of arena.
 static const uint32_t kCellArenaSize = kNumCells*4*sizeof(Cell*);
 static const uint32_t kCarArenaSize = kNumCars*10*sizeof(Cell*);
 static const uint32_t kSharedSignalGroupArenaSize =
@@ -29,6 +33,7 @@ static const uint32_t kTrafficLightArenaSize =
 static const uint32_t kPriorityCtrlArenaSize =
     kNumPriorityCtrls*8*sizeof(SharedSignalGroup*);
 
+// Kind of arrays.
 #define ARRAY_CELL_IS_PARTIAL
 #define ARRAY_CAR_IS_PARTIAL
 #define ARRAY_SIGNAL_GROUP_IS_PARTIAL
