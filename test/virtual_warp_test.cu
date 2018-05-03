6aa5007b80c53dc39c3b838df92565d9bdb4d549
Matthias Springer <me@matthiasspringer.de>
2018-05-03 14:12:58 +0900

    First version of iterator working.

--- test/virtual_warp_test.cu
+++ test/virtual_warp_test.cu
@@ -12,15 +12,23 @@ using ikra::executor::cuda::KernelConfig;
 using ikra::executor::cuda::KernelConfiguration;
 
 const static int kTestSize = 12;
+const static int kInnerArraySize = 4;
 
 class DummyClass : public SoaLayout<DummyClass, 1000> {
  public:
   IKRA_INITIALIZE_CLASS
 
-  __host__ __device__ DummyClass(int f0, int f1) : field0(f0), field1(f1) {}
+  __host__ __device__ DummyClass(int f0, int f1) : field0(f0), field1(f1) {
+    // Initialize array.
+    for (int i = 0; i < kInnerArraySize; ++i) {
+      nested_array_[i] = 100*id() + i;
+    }
+  }
 
   int_ field0;
   int_ field1;
+
+  array_(int, kInnerArraySize, fully_inlined) nested_array_;
 
   template<int Ikra_VW_SZ>
   __device__ void add_fields(int increment) {
@@ -30,6 +38,13 @@ class DummyClass : public SoaLayout<DummyClass, 1000> {
   template<int Ikra_VW_SZ>
   __device__ void add_fields_no_arg() {
     field0 = field0 + field1 + this->id() + Ikra_VW_SZ;
+  }
+
+  template<int Ikra_VW_SZ>
+  __device__ void update_array_vw(int val) {
+    for (int& el : nested_array_.vw_iterator<Ikra_VW_SZ>()) {
+      el += 10 + val;
+    }
   }
 };
 
@@ -78,6 +93,33 @@ void run_test_outer_cuda_execute_configuration_no_arg() {
     int actual = DummyClass::get(i)->field0;
     int expected = 5 + 6 + i + 4;
     EXPECT_EQ(actual, expected);
+  }
+
+  // Copy size to host memory and compare.
+  EXPECT_EQ(DummyClass::size(), static_cast<IndexType>(kTestSize));
+
+  // Make sure that we had no CUDA failures.
+  gpuErrchk(cudaPeekAtLastError());
+}
+
+void run_test_range_based_for_loop() {
+  DummyClass::initialize_storage();
+  EXPECT_EQ(DummyClass::size(), 0UL);
+
+  DummyClass* first = construct<DummyClass>(kTestSize, 5, 6);
+  gpuErrchk(cudaPeekAtLastError());
+
+  // Use a virtual warp size of 4.
+  cuda_execute_vw(&DummyClass::update_array_vw, KernelConfiguration<4>(12),
+                  first, 12, 20);
+
+  // Check result.
+  for (int i = 0; i < kTestSize; ++i) {
+    for (int j = 0; j < kInnerArraySize; ++j) {
+      int actual = DummyClass::get(i)->nested_array_[j];
+      int expected = 100*i + j + 10 + 20;
+      EXPECT_EQ(actual, expected);
+    }
   }
 
   // Copy size to host memory and compare.
@@ -151,3 +193,8 @@ TEST(VirtualWarpTest, TestOuterCudaExecuteNone) {
 TEST(VirtualWarpTest, TestOuterCudaExecuteConfiguration) {
   run_test_outer_cuda_execute_configuration();
 }
+
+TEST(VirtualWarpTest, TestRangeBasedForLoop) {
+  run_test_range_based_for_loop();
+}
+
